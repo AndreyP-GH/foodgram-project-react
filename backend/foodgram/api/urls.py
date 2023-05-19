f453a80fdcdfacbf9091ae0044b1c7c8b9ee9ab8
Andrey Pechnikov <pechnikovandrey@yandex.ru>
2023-05-19 17:53:47 +0300

    diploma_first iteration

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -16,5 +16,5 @@ router.register('users', CustomUserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include('djoser.urls.authtoken',))
+    path('auth/', include('djoser.urls.authtoken'))
 ]
